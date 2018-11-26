22be0d9068cf654e9ae63d8680ec04b5feaeed79
Ace Chua <acecyq@gmail.com>
2018-11-27 07:23:18 +0800

    Add button and modal to create exercise. Going to add textfield for form input.

--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import Create from '../../Exercises/Dialog/Create';
 
-export default props =>
-	<div>
-     	<AppBar position="static" gutterbottom='true'>
-        	<Toolbar>
-          		<Typography variant="h4" color="inherit">
-            		Exercise Database
-          		</Typography>
-        	</Toolbar>
-      	</AppBar>
-    </div>
+export default props => {
+	return (
+		<div>
+			<AppBar position="static" gutterbottom='true'>
+				<Toolbar>
+					<Typography 
+						variant="h4" 
+						color="inherit"
+						style={{flex: 1}}
+					>
+						Exercise Database
+					</Typography>
+					<Create />
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+}
